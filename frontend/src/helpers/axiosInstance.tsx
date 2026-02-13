b7628aa16e17ddd3508269c20272ec21fe1c4864
Saksham <saxenasaksham46@gmail.com>
2026-02-13 16:01:39 +0530

    Added Minor Fixes

--- frontend/src/helpers/axiosInstance.tsx
+++ frontend/src/helpers/axiosInstance.tsx
@@ -1,17 +1,22 @@
-import axios from 'axios'
+import axios from 'axios';
+
+// Axios instance for general API requests
+
+const API_URL = import.meta.env.VITE_ENVIRONMENT === 'production' ? import.meta.env.VITE_API_URL : 'http://localhost:8000';
 
 const publicInstance = axios.create({
-    baseURL: 'http://localhost:8000/',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-})
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
+// Axios instance for file uploads
 const fileUploadInstance = axios.create({
-    baseURL: 'http://localhost:8000/',
-    headers: {
-        'Content-Type': 'multipart/form-data',
-    },
-})
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
+});
 
-export { fileUploadInstance ,publicInstance }
+export { publicInstance, fileUploadInstance };
